599f94be1a916c5dc03219673534dafbb76c84be
Ignacio Rossetto <ignaciorossetto@hotmail.com>
2023-08-18 19:19:49 -0300

    home page v1.1

--- src/index.js
+++ src/index.js
@@ -10,6 +10,8 @@ app.use(cors('*'))
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 
+
+app.use('/api/users', usersRoute)
 app.use('/', (req, res) => {
     res.json({
         title: 'Proyecto hospital',
@@ -25,8 +27,6 @@ app.use('/', (req, res) => {
     }}}
     )
 })
-
-app.use('/api/users', usersRoute)
 
 const port = 8000
 
